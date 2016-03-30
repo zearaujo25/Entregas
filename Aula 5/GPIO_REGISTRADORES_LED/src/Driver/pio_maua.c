9de64815c343f7e43ab095fffbe483608fa23d11
zearaujo <zearaujo>
2016-03-30 17:54:48 -0300

    PAROU DE FUNCIONAR

---
+++ Aula 5/GPIO_REGISTRADORES_LED/src/Driver/pio_maua.c
@@ -0,0 +1,69 @@
+/*
+ * pio_maua.c
+ *
+ * Created: 16/03/2016 17:57:12
+ *  Author: 11.01298-6
+ */ 
+
+#include <asf.h>
+#include "pio_maua.h"
+
+void _pio_set_output(	Pio *p_pio,const uint32_t ul_mask,const uint32_t ul_default_level,const uint32_t ul_pull_up_enable){
+	p_pio->PIO_PER = (1 << ul_mask);
+	p_pio->PIO_WPMR = 0;
+	p_pio->PIO_OER  = (1 << ul_mask );
+	
+	// Acionamento de pull up
+	_pio_pull_up(p_pio,ul_mask,ul_pull_up_enable);
+	
+	
+	//setando o valor padrao 
+	if(ul_default_level == 0)
+	{
+		p_pio->PIO_CODR = (1 << ul_mask );
+		
+	}
+	else
+	{
+		p_pio->PIO_SODR = (1 << ul_mask );
+		
+	}
+}
+
+
+
+void _pio_set_input( 	Pio *p_pio,const uint32_t ul_mask,const uint32_t ul_attribute){
+	
+	//setando o pino como input
+		p_pio->PIO_PER = (ul_mask);
+		p_pio->PIO_WPMR = 0;
+		p_pio->PIO_ODR  = (ul_mask );
+		_pio_pull_up(p_pio,ul_mask,1);
+	//Confirguracoes de debounce e deglitch
+	
+	
+	if(ul_attribute & ( 1<< PIO_DEBOUNCE)){
+	p_pio->PIO_IFER = ( ul_mask);
+	p_pio->PIO_IFSCER = (ul_mask);
+	
+	}
+	if(ul_attribute & ( 1<< PIO_DEGLITCH)){
+		p_pio->PIO_IFER = (ul_mask);
+		p_pio->PIO_IFSCDR = ( ul_mask);
+	}
+		
+	
+}
+void _pio_pull_up(	Pio *p_pio,const uint32_t ul_mask,const uint32_t ul_pull_up_enable){
+	if(ul_pull_up_enable==1)
+		p_pio->PIO_PUER = (ul_mask );
+	else
+		p_pio->PIO_PUDR = (ul_mask );	
+}
+
+void _pio_pull_down( Pio *p_pio,const uint32_t ul_mask,const uint32_t ul_pull_down_enable){
+	if(ul_pull_down_enable==1)
+		p_pio->PIO_PPDER = (ul_pull_down_enable << ul_mask );
+	else
+		p_pio->PIO_PPDDR = (1 << ul_mask );
+}
---
+++ Aula 5/GPIO_REGISTRADORES_LED/src/main.c
@@ -0,0 +1,137 @@
+/**
+ * IMT - Rafael Corsi
+ * 
+ * PIO - 07
+ *  Configura o PIO do SAM4S (Banco A, pino 19) para operar em
+ *  modo de output. Esse pino está conectado a um LED, que em 
+ *  lógica alta apaga e lógica baixa acende.
+*/
+
+#include <asf.h>
+#include "Driver/pio_maua.h"
+#include "Driver/pmc_maua.h"
+
+/*
+ * Prototypes
+ */
+
+/** 
+ * Definição dos pinos
+ * Pinos do uC referente aos LEDS.
+ *
+ * O número referente ao pino (PIOAxx), refere-se ao
+ * bit que deve ser configurado no registrador para alterar
+ * o estado desse bit específico.
+ *
+ * exe : O pino PIOA_19 é configurado nos registradores pelo bit
+ * 19. O registrador PIO_SODR configura se os pinos serão nível alto.
+ * Nesse caso o bit 19 desse registrador é referente ao pino PIOA_19
+ *
+ * ----------------------------------
+ * | BIT 19  | BIT 18  | ... |BIT 0 |
+ * ----------------------------------
+ * | PIOA_19 | PIOA_18 | ... |PIOA_0|
+ * ----------------------------------
+ */
+#define PIN_LED_BLUE 19
+#define PIN_LED_RED 20
+#define PIN_LED_GREEN 20
+#define PIN_PUSHBUTTON_2 3
+/** 
+ * Definição dos ports
+ * Ports referentes a cada pino
+ */
+#define PORT_LED_BLUE PIOA
+#define PORT_LED_RED PIOC
+#define PORT_LED_GREEN PIOA
+#define PORT_PUSHBUTTON_2 PIOB
+
+/**
+ * Main function
+ * 1. configura o clock do sistema
+ * 2. desabilita wathdog
+ * 3. ativa o clock para o PIOA
+ * 4. ativa o controle do pino ao PIO
+ * 5. desabilita a proteção contra gravações do registradores
+ * 6. ativa a o pino como modo output
+ * 7. coloca o HIGH no pino
+ */
+
+int main (void)
+{
+
+	/**
+	* Inicializando o clock do uP
+	*/
+	sysclk_init();
+	
+	/** 
+	*  Desabilitando o WathDog do uP
+	*/
+	WDT->WDT_MR = WDT_MR_WDDIS;
+		
+	// 29.17.4 PMC Peripheral Clock Enable Register 0
+	// 1: Enables the corresponding peripheral clock.
+	// ID_PIOA = 11 - TAB 11-1
+	//PMC->PMC_PCER0 |= (1<<ID_PIOA) | (1<<ID_PIOB);
+	_pmc_enable_clock_periferico(ID_PIOA);
+	_pmc_enable_clock_periferico(ID_PIOB);
+	
+	//PMC->PMC_PCER0 |= ID_PIOC;
+	 //31.6.1 PIO Enable Register
+	// 1: Enables the PIO to control the corresponding pin (disables peripheral control of the pin).	
+	//PIOA->PIO_PER = (1 << PIN_LED_BLUE );
+	//PIOB->PIO_PER = (1 << PIN_PUSHBUTTON_2 );
+	
+	//PIOA->PIO_PER |= (1 << PIN_LED_GREEN );
+	//PIOC->PIO_PER |= (1 << PIN_LED_RED );
+	// 31.6.46 PIO Write Protection Mode Register
+	// 0: Disables the write protection if WPKEY corresponds to 0x50494F (PIO in ASCII).
+	//PIOA->PIO_WPMR = 0;
+	//PIOB->PIO_WPMR = 0;
+	
+	//PIOB->PIO_ODR  = (1 << PIN_PUSHBUTTON_2 );
+	//PIOB->PIO_PUER = (1 << PIN_PUSHBUTTON_2 );
+
+	
+	// 31.6.4 PIO Output Enable Register
+	// 1: Enables the output on the I/O line.
+	//PIOA->PIO_OER |=  (1 << PIN_LED_BLUE );
+	//PIOA->PIO_OER |=  (1 << PIN_LED_GREEN );
+	//PIOC->PIO_OER |=  (1 << PIN_LED_RED );
+
+	// 31.6.10 PIO Set Output Data Register
+	// 1: Sets the data to be driven on the I/O line.
+	
+	_pio_set_output(PIOA,(1 << PIN_LED_BLUE ), 1, 0);
+	_pio_set_input(PIOB,1<<PIN_PUSHBUTTON_2,1);
+
+	/**
+	*	Loop infinito
+	*/
+		while(1){
+		
+            /*
+             * Utilize a função delay_ms para fazer o led piscar na frequência
+             * escolhida por você.
+             */
+			
+		//if(((PIOB->PIO_PDSR >> PIN_PUSHBUTTON_2) & 1) == 0)
+		//{
+			//_pio_set(PIOA,1<<PIN_LED_BLUE);
+		//	PIOA->PIO_SODR =  (1 << PIN_LED_BLUE );
+		//}
+		//else
+		{
+			//_pio_clear(PIOA,1<<PIN_LED_BLUE);
+			//_pio_set(PIOA,1<<PIN_LED_BLUE);
+			PIOA->PIO_CODR =  (1 << PIN_LED_BLUE );
+			
+		}	
+		
+		
+	}
+}
+
+
+
